a894f1bc8a789cfa90cdda000444f8e0500d6016
cesmiley <catherinesmiley@gmail.com>
2020-09-29 19:37:00 -0400

    add seed data and include associated category in items controller index action

--- server-side/app/controllers/items_controller.rb
+++ server-side/app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
 
-    render json: @items
+    render json: @items, include: [:category]
   end
 
   # GET /items/1
--- server-side/db/seeds.rb
+++ server-side/db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+camping = Category.create(name: "Camping", description: "Going camping? Make sure you have everything on this list!")
+camping.items.create([{ name: "tent" }, { name: "sleeping bag" }, {name: "flashlight" }, { name: "soap" }])
+
+road_trip = Category.create(name: "Road Trip", description: "Tip: wear comfy clothes!")
+road_trip.items.create([{ name: "cash for tolls" }, { name: "portable or car charger" }, { name: "water" }, { name: "snacks" }])
