097dad4b45a95dd2b2cbf40716083f2121893ccb
cesmiley <catherinesmiley@gmail.com>
2020-10-01 19:08:13 -0400

    create categories container and move code from app file

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,36 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { fetchCategories } from './actions/categories'
+import CategoriesContainer from './components/CategoriesContainer'
 
 class App extends Component {
 
-    componentDidMount() {
-        this.props.fetchCategories()
-    }
-
     render() {
-        console.log("categories in render", this.props.categories)
-        let categories = this.props.categories.map((category, index) => 
-            <div key={index}>
-                <h2>{category.name}</h2>
-                <h4>{category.description}</h4>
-                <ul>
-                    <li>{category.items.name}</li>
-                </ul>
-            </div>
-        )
         return (
             <div>
-                {categories}
+                <CategoriesContainer />
             </div>
         );
     }
 }
 
-const mapStateToProps = state => {
-    return {
-        categories: state.categories
-    }
-}
-
-export default connect(mapStateToProps, { fetchCategories })(App);
+export default App;
---
+++ client-side/src/components/CategoriesContainer.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { fetchCategories } from '../actions/categories'
+
+class CategoriesContainer extends Component {
+
+    componentDidMount() {
+        this.props.fetchCategories()
+    }
+
+    render() {
+        console.log("categories in render", this.props.categories)
+        let categories = this.props.categories.map((category, index) => 
+            <div key={index}>
+                <h2>{category.name}</h2>
+                <h4>{category.description}</h4>
+                <ul>
+                    <li>{category.items.name}</li>
+                </ul>
+            </div>
+        )
+        return (
+            <div>
+                {categories}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        categories: state.categories
+    }
+}
+
+export default connect(mapStateToProps, { fetchCategories })(CategoriesContainer);
