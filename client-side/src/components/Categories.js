2c01d969df6fa062adcba1f7607e30cce3e95e54
cesmiley <catherinesmiley@gmail.com>
2020-10-05 23:10:43 -0400

    add links to categories component

--- client-side/src/components/Categories.js
+++ client-side/src/components/Categories.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 
 const Categories = props => {
     const categories = props.categories.map((category, index) => {
+        console.log("category items in map", category.items)
         return <div key={index}>
-            <h2>{category.name}</h2>
+            <NavLink
+                to="/categories"
+                exact
+            ><h2>{category.name}</h2></NavLink>
             <h4>{category.description}</h4>
+            {/* {categories.map(function(item, index) {
+                return (<li key={index}>{item}</li>)
+            })} */}
         </div>
         // render each item
     })
 
+    // const items = categories.map((item, index) => {
+    //     console.log("item", item)
+    //     return (<li key={index}>{item}</li>)
+    // })
+
     return (
         <div>
             {categories}
+            {/* {items} */}
         </div>
     );
 };
---
+++ client-side/src/components/CategoryItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CategoryItems = props => {
+    const items = props.categories.map
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CategoryItems;
