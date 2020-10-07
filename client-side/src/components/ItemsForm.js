1c0f6dafcdb2a757458f469e1ca82a2b2a05fb7c
cesmiley <catherinesmiley@gmail.com>
2020-10-07 13:20:26 -0400

    add onsubmit and onchange attributes and methods to items form

--- client-side/src/components/ItemsForm.js
+++ client-side/src/components/ItemsForm.js
@@ -5,17 +5,35 @@ class ItemsForm extends Component {
 
     state = {
         name: "", 
-        category_id: ""
+        // category_id: ""
+    }
+
+    handleOnChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleOnSubmit = event => {
+        event.preventDefault()
+        const item = {
+            name: this.state.name
+            // add id?
+            // add id of associated category?
+        }
+        console.log("item on submit", item)
     }
 
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={event => this.handleOnSubmit(event)}>
                     <input
                         type="text"
                         placeholder="New item name"
                         name="name"
+                        onChange={this.handleOnChange}
+                        value={this.state.name}
                     />
                     <br />
                     <input type="submit" value="Add new item"/>
