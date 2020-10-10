721701b573d3139844db4efe75927ac37306e13d
cesmiley <catherinesmiley@gmail.com>
2020-10-10 01:35:54 -0400

    font color, link color, background color, and text alignment styling

--- client-side/src/components/Categories.js
+++ client-side/src/components/Categories.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Categories = props => {
     const categories = props.categories.categories.map((category, index) => {
-        return <div key={index}>
+        return <div key={index} className="catList">
             <Link
                 to={`/categories/${category.id}`}
             ><h2>{category.name}</h2></Link>
