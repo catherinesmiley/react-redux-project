eba6180dbc524e8da74942314e701bdd61d1b2af
cesmiley <catherinesmiley@gmail.com>
2020-10-10 23:17:00 -0400

    resize gif and add h3 styling

--- client-side/src/components/Home.js
+++ client-side/src/components/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div>
             <h1>Checklist Library Home Page</h1>
-            <iframe src="https://giphy.com/embed/pOD7xmRum8R6o" width="235" height="240" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/forget-pOD7xmRum8R6o">via GIPHY</a></p>
+            <iframe src="https://giphy.com/embed/pOD7xmRum8R6o" width="282" height="288" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/forget-pOD7xmRum8R6o">via GIPHY</a></p>
             <h2>This doesn't have to be you!</h2>
             <h3>Going camping for the first time? Offered to host the next family BBQ but have no idea what you're doing? You can succeed in any new situation, you just have to be prepared. For that, all you need is a checklist!</h3>
             <h3>Click on All Categories to select from our library of pre-made checklists, then click the name of the checklist to see all the items on the list. From there you can check off things as you go so you only see what's left!</h3>
